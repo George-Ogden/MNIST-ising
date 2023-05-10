f470383b70ccb6e6047d6d6105690945d01312a6
George Ogden <go281@srcf.net>
2023-05-10 16:02:41 +0100

    add a generation function

--- src/ising/model.py
+++ src/ising/model.py
@@ -26,3 +26,17 @@ class IsingModel:
         self.count += 1
         self.nodes[sample] += 1
         self.edges[sample.reshape(1, -1) == sample.reshape(-1, 1)] += 1
+    
+    def generate(self, iterations: int = 100) -> np.ndarray:
+        sample = np.sign(np.random.randn(np.prod(self.shape)))
+        sample[sample == 0] = 1
+        # $H(\sigma) = -J \sum_{i,j} \sigma_i \sigma_j - h \sum_i \sigma_i$
+        J = (self.edges / self.count) * 2 - 1
+        h = (self.nodes / self.count) * 2 - 1
+        for _ in range(iterations):
+            indices = np.random.permutation(len(sample))
+            for index in indices:
+                delta_energy = h[index] * sample[index] + 2 * J[index] * sample * sample[index]
+                if (delta_energy).sum() <= 0:
+                    sample[index] *= -1
+        return sample.reshape(self.shape) > 0
