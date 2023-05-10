eeb2d01b60504041d858f468b4c8430498fdfab6
George Ogden <go281@srcf.net>
2023-05-10 14:52:45 +0100

    implement simpler ising model fit

--- src/ising/model.py
+++ src/ising/model.py
@@ -1,3 +1,4 @@
+from tqdm import trange
 import numpy as np
 
 from typing import Tuple
@@ -8,19 +9,20 @@ class IsingModel:
     def __init__(self, shape: Tuple[int, ...]):
         self.shape = shape
         nodes = np.prod(self.shape)
-        self.edges = np.zeros((nodes, nodes, 2), dtype=int)
-        self.nodes = np.zeros((nodes, 2), dtype=int)
+        self.edges = np.zeros((nodes, nodes), dtype=int)
+        self.nodes = np.zeros((nodes), dtype=int)
+        self.count = 0
 
-    def fit(self, dataset: Jsb16thSeparatedDataset):
-        for epoch in range(10):
+    def fit(self, dataset: Jsb16thSeparatedDataset, epochs: int = 10):
+        for epoch in trange(epochs, desc="Fitting Ising Model"):
             for i in range(len(dataset)):
                 sample = dataset[i]
                 assert sample.dtype == bool, "samples must be binary"
                 assert sample.shape == self.shape, "all samples must have the same shape"
                 self._update(sample)
-        self.edges += self.edges.T
-        self.edges += 1
     
     def _update(self, sample: np.ndarray):
-        sample = sample.reshape(-1).astype(int)
-        self.nodes[np.arange(len(sample)), sample] += 1
+        sample = sample.reshape(-1)
+        self.count += 1
+        self.nodes[sample] += 1
+        self.edges[sample.reshape(1, -1) == sample.reshape(-1, 1)] += 1
