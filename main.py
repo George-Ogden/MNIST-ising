a2bf8291a1323db562bbfe1deafba5a8b2083a07
George Ogden <go281@srcf.net>
2023-05-10 16:33:46 +0100

    train on training set

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from src.ising import IsingModel
 
 if __name__ == "__main__":
     factory = Jsb16thSeparatedDatasetFactory()
-    dataset = factory.val_dataset
+    dataset = factory.train_dataset
 
     model = IsingModel(dataset[0].shape)
-    model.fit(dataset, epochs=10)
+    model.fit(dataset, epochs=5)
 
     sample = model.generate()
     dataset.info.save_pianoroll(sample, "sample.mid")
